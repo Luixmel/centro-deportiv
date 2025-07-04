9598b707dc9e2b41ed61adbe4daafa761d9b801a
luis fernando diaz medina <luisfernandofeliz1@gmail.com>
2025-07-04 18:25:14 -0500

    feat: implementar controlador y servicio para gestión de usuarios con operaciones CRUD

--- src/usuario/usuario.module.ts
+++ src/usuario/usuario.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
-import { UsuarioController } from './usuario.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Usuario } from './entities/usuario.entity';
 import { UsuarioService } from './usuario.service';
+import { UsuarioController } from './usuario.controller';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Usuario])],
   controllers: [UsuarioController],
   providers: [UsuarioService],
 })
--- src/usuario/usuario.service.ts
+++ src/usuario/usuario.service.ts
@@ -1,4 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Usuario } from './entities/usuario.entity';
+import { CreateUsuarioDto } from './dto/create-usuario.dto';
+import { UpdateUsuarioDto } from './dto/update-usuario.dto';
 
 @Injectable()
-export class UsuarioService {}
+export class UsuarioService {
+  constructor(
+    @InjectRepository(Usuario)
+    private readonly usuarioRepository: Repository<Usuario>,
+  ) {}
+
+  async create(createUsuarioDto: CreateUsuarioDto): Promise<Usuario> {
+    const nuevoUsuario = this.usuarioRepository.create(createUsuarioDto);
+    return this.usuarioRepository.save(nuevoUsuario);
+  }
+
+  findAll(): Promise<Usuario[]> {
+    return this.usuarioRepository.find({ relations: ['reservas'] });
+  }
+
+  async findOne(id: number): Promise<Usuario> {
+    const usuario = await this.usuarioRepository.findOne({
+      where: { id },
+      relations: ['reservas'],
+    });
+    if (!usuario) {
+      throw new NotFoundException(`Usuario con id ${id} no encontrado`);
+    }
+    return usuario;
+  }
+
+  async update(
+    id: number,
+    updateUsuarioDto: UpdateUsuarioDto,
+  ): Promise<Usuario> {
+    const usuario = await this.findOne(id);
+    const actualizado = Object.assign(usuario, updateUsuarioDto);
+    return this.usuarioRepository.save(actualizado);
+  }
+
+  async remove(id: number): Promise<void> {
+    const usuario = await this.findOne(id);
+    await this.usuarioRepository.remove(usuario);
+  }
+}
